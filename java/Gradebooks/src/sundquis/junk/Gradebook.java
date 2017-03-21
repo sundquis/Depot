8edd641024c3cf3a41c2bc531e668452d5cf5808
Thomas Sundquist <thomas.sundquist@gmail.com>
2017-03-20 20:25:11 -0500

    Clean

--- java/Gradebooks/src/sundquis/junk/Gradebook.java
+++ java/Gradebooks/src/sundquis/junk/Gradebook.java
@@ -5,49 +5,49 @@ import java.io.File;
 
 public class Gradebook {
 	
-	private final Workbook wb;
+//	private final Workbook wb;
 
-	private final String year;
-	private final String term;
-	private final String course;
+//	private final String year;
+//	private final String term;
+//	private final String course;
 	
-	private int curRecord;
-	private int lastRecord;
+//	private int curRecord;
+//	private int lastRecord;
 	
-	public Gradebook( String path ) {
-		super();
+//	public Gradebook( String path ) {
+//		super();
 		
-		this.wb = Workbook.getWorkbook( new File( path ) );
+//		this.wb = Workbook.getWorkbook( new File( path ) );
 
-		// TODO parse
-		this.year = null;
-		this.term = null;
-		this.course = null;
+//		// TODO parse
+//		this.year = null;
+//		this.term = null;
+//		this.course = null;
 		
-		this.init();
-	}
+//		this.init();
+//	}
 	
-	// Initialize column statistics etc
-	private void init() {
-		// TODO
-		this.curRecord = 0;
-		this.lastRecord = 0;
-	}
+//	// Initialize column statistics etc
+//	private void init() {
+//		// TODO
+//		this.curRecord = 0;
+//		this.lastRecord = 0;
+//	}
 	
 	// Advance to next record, return true if one exists
-	public abstract boolean hasRecord();
+//	public abstract boolean hasRecord();
 	
-	public String getRecord() {
-		return this.year 
-				+ "\t" + this.term 
-				+ "\t" + this.course 
-				+ "\t" + this.id
-				+ "\t" + this.last
-				+ "\t" + this.first
-				+ "\t" + this.grade
-				+ "\t" + this.score
-				+ "\t" + this.rank;
-	}
+//	public String getRecord() {
+//		return this.year 
+//				+ "\t" + this.term 
+//				+ "\t" + this.course 
+//				+ "\t" + this.id
+//				+ "\t" + this.last
+//				+ "\t" + this.first
+//				+ "\t" + this.grade
+//				+ "\t" + this.score
+//				+ "\t" + this.rank;
+//	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
