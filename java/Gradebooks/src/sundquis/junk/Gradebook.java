e9e425d10e50188f60ddd40c66f884eeb3ac03a7
Thomas Sundquist <thomas.sundquist@gmail.com>
2017-03-22 13:58:36 -0500

    First working version

--- java/Gradebooks/src/sundquis/junk/Gradebook.java
+++ java/Gradebooks/src/sundquis/junk/Gradebook.java
@@ -1,56 +1,57 @@
 package sundquis.junk;
 
 import jxl.Workbook;
+import jxl.Sheet;
+import jxl.Cell;
+
 import java.io.File;
 
 public class Gradebook {
 	
-//	private final Workbook wb;
-
-//	private final String year;
-//	private final String term;
-//	private final String course;
-	
-//	private int curRecord;
-//	private int lastRecord;
-	
-//	public Gradebook( String path ) {
-//		super();
+	public static void display( String path ) throws Exception {
+		File file = new File( path );
+		Workbook wb = Workbook.getWorkbook( file );
+		Sheet sheet = wb.getSheet( 0 );
+		Cell[] cells = sheet.getRow( 5 );
+		int numCols = cells.length;
+		for ( int j = 0; j < numCols; j++ ){
+			cells = sheet.getColumn( j );
+			System.out.printf( "%4.3s", String.valueOf(j+1) );
+			for ( int i = 0; i < Math.min( cells.length, 10); i++ ) {
+				System.out.printf( "%8.7s", cells[i].getContents() );
+			}
+			System.out.println();;
+		}
 		
-//		this.wb = Workbook.getWorkbook( new File( path ) );
-
-//		// TODO parse
-//		this.year = null;
-//		this.term = null;
-//		this.course = null;
-		
-//		this.init();
-//	}
+		wb.close();
+	}
 	
-//	// Initialize column statistics etc
-//	private void init() {
-//		// TODO
-//		this.curRecord = 0;
-//		this.lastRecord = 0;
-//	}
-	
-	// Advance to next record, return true if one exists
-//	public abstract boolean hasRecord();
-	
-//	public String getRecord() {
-//		return this.year 
-//				+ "\t" + this.term 
-//				+ "\t" + this.course 
-//				+ "\t" + this.id
-//				+ "\t" + this.last
-//				+ "\t" + this.first
-//				+ "\t" + this.grade
-//				+ "\t" + this.score
-//				+ "\t" + this.rank;
-//	}
+	public static String[] GB = {
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.1.FALL/1500/gradebook.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.1.FALL/1400/gradebook.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.1.FALL/700/grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.3.SUMMER/1510/grades2.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.3.SUMMER/1510/grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.2.SPRING/1100/Grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/03-04/03-04.2.SPRING/1500/Grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/07-08/07-08.1.FALL/MC14.16/Grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/07-08/07-08.1.FALL/1500/Grades.xls",
+		"/home/sundquis/book/Dropbox/tmp/07-08/07-08.2.SPRING/1510/Grades.xlsx",
+		"/home/sundquis/book/Dropbox/tmp/07-08/07-08.3.SUMMER/1500/Grades.xlsx",
+		"/home/sundquis/book/Dropbox/tmp/07-08/07-08.3.SUMMER/2700/Grades.xlsx",
+		"/home/sundquis/book/Dropbox/tmp/16-17/16-17.1.FALL/1520/Grades.1520.30.xlsx",
+		"/home/sundquis/book/Dropbox/tmp/16-17/16-17.1.FALL/1500/Grades.1500.90.xlsx",
+		"/home/sundquis/book/Dropbox/tmp/16-17/16-17.1.FALL/2011/Grades.2011.10.xlsx"
+	};
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println( "MAIN" );
+		
+		try {
+			display( args[0] );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
